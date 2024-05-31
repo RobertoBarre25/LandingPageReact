d07019d91b1b93a9af5e395a06b83973560b4815
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-05-31 11:08:10 -0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './NavBar/Header';
+import Footer from './footer/footer';
 
 function App() {
   return (
     <div className="App">
-       <Header/>
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         
         </p>
         <a
           className="App-link"
@@ -20,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
