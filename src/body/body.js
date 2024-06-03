582d310b35b73807eafbbc21d40df09ae97a2cb3
Dari175 <arturdar16@gmail.com>
2024-06-03 12:05:49 -0600

    componente card importado en body y eliminado en el app principal

--- src/body/body.js
+++ src/body/body.js
@@ -1,6 +1,8 @@
 // body.js
 import React from 'react';
 import './body.css';
+import HorizontalCard from '../CardShadow/HorizontalCard';
+
 
 const Body = () => {
   return (
@@ -14,6 +16,12 @@ const Body = () => {
         <h1 className="primerTitulo">¿Necesitas una Red de Datos?</h1>
         <p className='textoSecundario'>En Grupo Alternativas y Soluciones te ofrecemos un servicio profesional</p>
       </div>
+      <HorizontalCard
+          imageSrc="https://fundacioncarlosslim.org/wp-content/uploads/2016/11/redes-de-datos.jpg"
+          title="Redes de Datos"
+          description="¿Quieres saber sobre los servicios de Redes de Datos?
+            En Grupo Alternativas y Soluciones te ofrecemos un servicio profesional."
+        />
     </div>
   );
 };
