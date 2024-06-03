175356698ae1304980d6dbb1c0451b15601f8b0b
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-06-03 17:53:06 -0600

    Carousel

--- src/body/body.js
+++ src/body/body.js
@@ -1,17 +1,18 @@
-// body.js
 import React from 'react';
 import './body.css';
 import HorizontalCard from '../CardShadow/HorizontalCard';
+import Carousel from '../Carousel/carousel'; // Asegúrate de importar el Carousel correctamente
 
+const images = [
+    'https://cdn-cemnc.nitrocdn.com/VpRCNzZxvcuRoMlvVcXWGPvNRVQJdDtQ/assets/images/optimized/rev-1959f9d/www.networkcablingservices.com/wp-content/uploads/2021/08/Next-Generation-Data-Centers-Everything-You-Need-to-Know.jpeg',
+    'https://engineering.fb.com/wp-content/uploads/2018/05/data-center-shot.jpg',
+    'https://img.jakpost.net/c/2022/04/01/2022_04_01_124204_1648793484._large.jpg'
+];
 
 const Body = () => {
   return (
     <div className="bodyPrincipal">
-      <img
-        className="imagenPrincipal"
-        src="https://hinforcom.com/wp-content/uploads/2023/07/Guia-sobre-antenas-de-cobertura-movil-scaled.jpg"
-        alt="Antenas de cobertura móvil"
-      />
+      <Carousel images={images} /> {/* Integra el Carousel aquí */}
       <div className="primerTexto">
         <h1 className="primerTitulo">¿Necesitas una Red de Datos?</h1>
         <p className='textoSecundario'>En Grupo Alternativas y Soluciones te ofrecemos un servicio profesional</p>
