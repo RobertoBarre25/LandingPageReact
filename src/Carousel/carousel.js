6a2fa9e669bd58b6b26330b735befd0bcda15737
Arturo <artur@VICTUS>
2024-06-05 09:13:13 -0600

    manejando tawlind en header y carousel

--- src/Carousel/carousel.js
+++ src/Carousel/carousel.js
@@ -1,35 +1,44 @@
-// src/carousel.js
 import React, { useState, useEffect } from 'react';
 import './carousel.css';
 
-const Carousel = ({ images }) => {
+function Carousel({ images }) {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [prevIndex, setPrevIndex] = useState(images.length - 1);
 
     useEffect(() => {
         const interval = setInterval(() => {
+            setPrevIndex(currentIndex);
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 7000);
+        }, 5000);
         return () => clearInterval(interval);
-    }, [images.length]);
+    }, [images.length, currentIndex]);
 
     const setSlide = (index) => {
+        setPrevIndex(currentIndex);
         setCurrentIndex(index);
     };
 
     return (
-        <div className="carousel">
-            <img src={images[currentIndex]} alt="carousel" className="carousel-image" />
-            <div className="carousel-indicators">
+        <div className="carousel relative w-full overflow-hidden">
+            {images.map((image, index) => (
+                <img
+                    key={index}
+                    src={image}
+                    alt="carousel"
+                    className={`carousel-image absolute w-full h-full transition-transform duration-700 ease-in-out ${index === currentIndex ? 'translate-x-0' : index === prevIndex ? '-translate-x-full' : 'translate-x-full'}`}
+                />
+            ))}
+            <div className="carousel-indicators absolute bottom-4 rigth-10 w-full text-center">
                 {images.map((_, index) => (
                     <span
                         key={index}
-                        className={`carousel-indicator ${index === currentIndex ? 'active' : ''}`}
+                        className={`carousel-indicator inline-block w-3 h-3 bg-white rounded-full mx-1 cursor-pointer  ${index === currentIndex ? 'bg-opacity-100' : 'bg-opacity-50'}`}
                         onClick={() => setSlide(index)}
                     ></span>
                 ))}
             </div>
         </div>
     );
-};
+}
 
 export default Carousel;
