aeb63a784adf8694881d141c8e656c2b39c9cb4f
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-06-05 14:20:42 -0600

    body

--- src/body/body.js
+++ src/body/body.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './body.css';
 import HorizontalCard from '../CardShadow/HorizontalCard';
-import Carousel from '../Carousel/carousel'; // Asegúrate de importar el Carousel correctamente
-import Cards from '../CardsSer/cards'; // Importa el componente Cards
+import Carousel from '../Carousel/carousel';
+import Cards from '../CardsSer/cards';
+import { FaAngleDown } from 'react-icons/fa'; // Importa el ícono de flecha hacia abajo
+import { animateScroll as scroll } from 'react-scroll'; // Importa la función de scroll suave
 
 const images = [
     'https://cdn-cemnc.nitrocdn.com/VpRCNzZxvcuRoMlvVcXWGPvNRVQJdDtQ/assets/images/optimized/rev-1959f9d/www.networkcablingservices.com/wp-content/uploads/2021/08/Next-Generation-Data-Centers-Everything-You-Need-to-Know.jpeg',
@@ -11,26 +13,32 @@ const images = [
 ];
 
 const Body = () => {
-  return (
-    <div className="bodyPrincipal">
-      <div className="carouselContainer">
-        <Carousel images={images} /> {/* Integra el Carousel aquí */}
-        <div className="carouselTextFondo">
-          <div className="carouselText">
-            <h1 className="primerTitulo">¿Nececitas una red de datos y ciberseguridad?</h1>
-            <p className='textoSecundario'>Manténgase actualizado con las últimas tendencias y novedades en redes de voz y datos a través de nuestro blog.</p>
-            <button className="botonServicios">Nuestros Servicios</button>
-          </div>
+    const scrollToBottom = () => {
+        scroll.scrollToBottom();
+    };
+
+    return (
+        <div className="bodyPrincipal">
+            <div className="carouselContainer">
+                <Carousel images={images} />
+                <div className="carouselTextFondo">
+                    <div className="carouselText">
+                        <h1 className="primerTitulo">¿Necesitas una red de datos y ciberseguridad?</h1>
+                        <p className='textoSecundario'>Manténgase actualizado con las últimas tendencias y novedades en redes de voz y datos a través de nuestro blog.</p>
+                        <button className="botonServicios" onClick={scrollToBottom}>
+                            Nuestros Servicios <FaAngleDown className="iconoV" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <HorizontalCard
+                imageSrc="https://fundacioncarlosslim.org/wp-content/uploads/2016/11/redes-de-datos.jpg"
+                title="Soluciones de redes de voz y datos de vanguardia para su empresa"
+                description="En Grupo Alternativas, ofrecemos soluciones de redes de voz y datos a medida, diseñadas para optimizar la comunicación y la productividad de su negocio. Nuestros expertos implementan las tecnologías más avanzadas, asegurando una conectividad confiable y segura"
+            />
+            <Cards />
         </div>
-      </div>
-      <HorizontalCard
-          imageSrc="https://fundacioncarlosslim.org/wp-content/uploads/2016/11/redes-de-datos.jpg"
-          title="Soluciones de redes de voz y datos de vanguardia para su empresa"
-          description="En Grupo Alternativas, ofrecemos soluciones de redes de voz y datos a medida, diseñadas para optimizar la comunicación y la productividad de su negocio. Nuestros expertos implementan las tecnologías más avanzadas, asegurando una conectividad confiable y segura"
-      />
-      <Cards />
-    </div>
-  );
+    );
 };
 
 export default Body;
