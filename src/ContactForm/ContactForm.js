ccbe667271562b2e9d561ea4fd84ebb7c7990e2d
RobertoBarre25 <robegamer25@gmail.com>
2024-06-06 18:10:29 -0600

    Fixed Form for contact

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -1,35 +1,150 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Header from '../NavBar/Header'; // Importa el componente Header
+import React from "react";
 
-const ContactForm = ({ setView }) => {
+const ContactForm = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <Header setView={setView} />
-      <form className="contact-form bg-white bg-opacity-75 p-8 rounded-lg shadow-md max-w-md z-10 relative">
-        <h2 className="text-center text-2xl font-bold mb-6">Solicitar Informacion del servicio</h2>
-        <div className="form-group mb-6">
-          <label htmlFor="name" className="block text-gray-700">Nombre</label>
-          <input type="text" id="name" name="name" required className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500" />
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="rounded-lg p-8 max-w-2xl w-full bg-transparent">
+        <div className="text-center mb-6">
+          <img
+            src="https://solucione.com.mx/corporativo/wp-content/uploads/2020/04/cropped-solucione-logo-color-512x512-1.png"
+            className="w-32 mx-auto"
+            alt="logo"
+          />
+          <h2 className="text-2xl font-bold">
+            Solicitar Información del Servicio
+          </h2>
         </div>
-        <div className="form-group mb-6">
-          <label htmlFor="email" className="block text-gray-700">Email</label>
-          <input type="email" id="email" name="email" required className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500" />
-        </div>
-        <div className="form-group mb-6">
-          <label htmlFor="message" className="block text-gray-700">Servicio Interesado</label>
-          <textarea id="message" name="message" rows="5" required className="w-full p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"></textarea>
-        </div>
-        <button type="submit" className="block w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300">Send</button>
-        <Link to="/" className="block w-full mt-4 text-center text-gray-700 font-bold py-2 px-4 rounded border border-gray-300 hover:bg-gray-300 transition duration-300 focus:outline-none focus:border-blue-500">
-          Back to Home
-        </Link>
-      </form>
-      <img
-        className="background-image absolute inset-0 object-cover blur-sm opacity-75 z-0"
-        src="https://hinforcom.com/wp-content/uploads/2023/07/Guia-sobre-antenas-de-cobertura-movil-scaled.jpg"
-        alt="Antenas de cobertura móvil"
-      />
+        <form className="space-y-6">
+          <div className="grid gap-6 md:grid-cols-2">
+            <input
+              type="text"
+              className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+              placeholder="Nombre(s)"
+            />
+            <input
+              type="text"
+              className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+              placeholder="Apellido(s)"
+            />
+            <input
+              type="text"
+              className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+              placeholder="País"
+            />
+            <input
+              type="text"
+              className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+              placeholder="Código Postal"
+            />
+          </div>
+          <div className="grid gap-6 md:grid-cols-2">
+            <input
+              type="number"
+              className="w-28 px-4 py-2 text-sm border border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none rounded-r-md"
+              placeholder="+ Lada"
+            />
+            <div className="relative flex items-center">
+              <input
+                type="number"
+                className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+                placeholder="Teléfono"
+              />
+            </div>
+          </div>
+          <div className="grid gap-6 md:grid-cols-2">
+            <div className="relative">
+              <input
+                type="email"
+                className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+                placeholder="Correo Electrónico"
+              />
+            </div>
+          </div>
+          <div className="relative">
+            <input
+              type="text"
+              className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+              placeholder="Nombre de la Ubicación o Vendedor Autorizado"
+            />
+          </div>
+          <div className="grid gap-6 md:grid-cols-2">
+            <div className="relative">
+              <input
+                type="date"
+                className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+              />
+            </div>
+            <div className="relative">
+              <input
+                type="time"
+                className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className="relative">
+            <label className="block text-sm text-gray-700">
+              ¿Deseas ofrecernos información adicional para ayudarnos a
+              conocerte mejor?
+            </label>
+            <div className="flex items-center justify-center mt-2">
+              <label className="mr-4">
+                <input
+                  type="radio"
+                  name="additionalInfo"
+                  className="mr-2"
+                />
+                Sí
+              </label>
+              <label>
+                <input type="radio" name="additionalInfo" className="mr-2" />
+                No
+              </label>
+            </div>
+          </div>
+          <div className="relative">
+            <label className="block text-sm text-gray-700">
+              <strong>Aceptar los términos y condiciones</strong>
+            </label>
+            <p className="text-sm text-gray-600 mb-2">
+              Antes de continuar, le rogamos leer nuestra nota informativa sobre
+              la privacidad. Después de haber leído y entendido la nota
+              informativa sobre la privacidad, autorizo el tratamiento de mis
+              datos personales por parte de Grupo Alternativas y Solucione:
+            </p>
+            <div className="flex items-start mb-2">
+              <input
+                type="checkbox"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+              />
+              <span className="ml-2 text-sm text-gray-700">
+                para actividades de marketing mediante correo electrónico
+                (boletín informativo), teléfono, SMS, MMS, chat, banner en
+                nuestros sitios y aplicaciones, mensajería instantánea, redes
+                sociales y correo tradicional
+              </span>
+            </div>
+            <div className="flex items-start mb-2">
+              <input
+                type="checkbox"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+              />
+              <span className="ml-2 text-sm text-gray-700">
+                para la creación de perfiles, destinados a la personalización de
+                ofertas e iniciativas de acuerdo con mis intereses y
+                preferencias
+              </span>
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
