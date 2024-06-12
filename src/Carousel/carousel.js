3204a446fefe26fdf999d22a18b1d8aa84ea6b55
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-06-12 17:40:33 -0600

    carousel responsive

--- src/Carousel/carousel.js
+++ src/Carousel/carousel.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { animateScroll as scroll } from 'react-scroll';
 
 function Carousel({ images }) {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,19 +20,21 @@ function Carousel({ images }) {
     };
 
     return (
-        <div className="relative max-w-full h-[990px] overflow-hidden">
+        <div className="relative w-full h-[70vh] sm:h-[80vh] md:h-[90vh] lg:h-[100vh] overflow-hidden">
             {images.map((image, index) => (
                 <div
                     key={index}
-                    className={`carousel-slide absolute w-full h-full transition-transform duration-700 ease-in-out ${index === currentIndex ? 'translate-x-0' : index === prevIndex ? '-translate-x-full' : 'translate-x-full'}`}
+                    className={`carousel-slide absolute top-0 left-0 w-full h-full transition-transform duration-700 ease-in-out 
+                        ${index === currentIndex ? 'translate-x-0' : index === prevIndex ? '-translate-x-full' : 'translate-x-full'}
+                    `}
                 >
                     <img
                         src={image.src}
-                        alt="carousel"
+                        alt={image.alt}
                         className="w-full h-full object-cover"
                     />
-                    <div className="overlay absolute inset-0 bg-black opacity-50"></div> {/* Superposición oscura */}
-                    <div className="carousel-text absolute inset-0 flex items-center justify-center text-center text-white">
+                    <div className="overlay absolute inset-0 bg-black opacity-50"></div>
+                    <div className="carousel-text absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
                         {image.text}
                     </div>
                 </div>
@@ -39,7 +43,9 @@ function Carousel({ images }) {
                 {images.map((_, index) => (
                     <span
                         key={index}
-                        className={`carousel-indicator inline-block w-3 h-3 bg-white rounded-full mx-1 cursor-pointer ${index === currentIndex ? 'bg-opacity-100' : 'bg-opacity-50'}`}
+                        className={`carousel-indicator inline-block w-3 h-3 bg-white rounded-full mx-1 cursor-pointer 
+                            ${index === currentIndex ? 'bg-opacity-100' : 'bg-opacity-50'}
+                        `}
                         onClick={() => setSlide(index)}
                     ></span>
                 ))}
