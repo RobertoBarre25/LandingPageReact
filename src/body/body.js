7643c1179c5897e9da19695597785717a382a548
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-06-12 16:49:43 -0600

    body

--- src/body/body.js
+++ src/body/body.js
@@ -3,14 +3,23 @@ import './body.css';
 import HorizontalCard from '../CardShadow/HorizontalCard';
 import Carousel from '../Carousel/carousel';
 import Cards from '../CardsSer/cards';
-import { animateScroll as scroll } from 'react-scroll'; // Importa la función de scroll suave
-import ImageSection from '../ImageSection/ImgSection'; // Importa el nuevo componente
-import { Link } from 'react-router-dom'; // Asegúrate de importar Link
+import { animateScroll as scroll } from 'react-scroll';
+import ImageSection from '../ImageSection/ImgSection';
+import { Link } from 'react-router-dom';
 
 const images = [
-    'https://cdn-cemnc.nitrocdn.com/VpRCNzZxvcuRoMlvVcXWGPvNRVQJdDtQ/assets/images/optimized/rev-1959f9d/www.networkcablingservices.com/wp-content/uploads/2021/08/Next-Generation-Data-Centers-Everything-You-Need-to-Know.jpeg',
-    'https://engineering.fb.com/wp-content/uploads/2018/05/data-center-shot.jpg',
-    'https://img.jakpost.net/c/2022/04/01/2022_04_01_124204_1648793484._large.jpg'
+    {
+        src: 'https://cdn-cemnc.nitrocdn.com/VpRCNzZxvcuRoMlvVcXWGPvNRVQJdDtQ/assets/images/optimized/rev-1959f9d/www.networkcablingservices.com/wp-content/uploads/2021/08/Next-Generation-Data-Centers-Everything-You-Need-to-Know.jpeg',
+        text: 'Texto descriptivo para la primera imagen'
+    },
+    {
+        src: 'https://engineering.fb.com/wp-content/uploads/2018/05/data-center-shot.jpg',
+        text: 'AntivirusCloud'
+    },
+    {
+        src: 'https://img.jakpost.net/c/2022/04/01/2022_04_01_124204_1648793484._large.jpg',
+        text: 'Texto descriptivo para la tercera imagen'
+    }
 ];
 
 const Body = () => {
@@ -24,23 +33,21 @@ const Body = () => {
                 <Carousel images={images} />
 
                 <div className="carouselTextFondo">
-                    
                     <div className="carouselText">
                         <h1 className="primerTitulo">
                             Antivirus<span className="cloudText">Cloud</span>
                         </h1>
                         <p className='textoSecundario'>Antivirus y antimalware empresarial</p>
                         <p className='textoSecundario'>
-                            Protección en tiempo real contra malware basada en la nube.</p>
-                        <Link to="/contact"
-
-                        >
+                            Protección en tiempo real contra malware basada en la nube.
+                        </p>
+                        <Link to="/contact">
                             <button className="botonServiciosAzul">
-                                Contrata ya !
+                                Contrata ya!
                             </button>
                         </Link>
                         <button className="botonServiciosTransparente" onClick={scrollToBottom}>
-                            Conoce más !
+                            Conoce más!
                         </button>
                     </div>
                 </div>
@@ -50,7 +57,6 @@ const Body = () => {
                 title="Soluciones de redes de voz y datos de vanguardia para su empresa"
                 description="En Grupo Alternativas, ofrecemos soluciones de redes de voz y datos a medida, diseñadas para optimizar la comunicación y la productividad de su negocio. Nuestros expertos implementan las tecnologías más avanzadas, asegurando una conectividad confiable y segura"
             />
-
             <Cards />
             <ImageSection />
         </div>
