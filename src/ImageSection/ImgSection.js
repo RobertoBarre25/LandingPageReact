7c0d33a3b29265f862b92153a917341d3f7d9d4f
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-06-13 10:00:20 -0600

    img reposive

--- src/ImageSection/ImgSection.js
+++ src/ImageSection/ImgSection.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const ImageSection = () => {
   return (
-    <div className="mx-10 md:mx-20 lg:mx-20"> 
+    <div className="px-4 md:px-8 lg:px-12 w-full"> 
       <div className="mt-20"> 
         <div className="flex flex-wrap justify-between">
           <div className="w-full sm:w-1/2 lg:w-1/3 mb-8">
             <div className="relative w-full h-64"> 
               <img src="https://i.pinimg.com/564x/c7/57/7c/c7577cb0fbd63a7ef1630a54829fae2c.jpg" alt="Configurador Ducati" className="w-full h-full object-cover" />
               <div className="absolute inset-0 bg-black opacity-75 transition-opacity duration-500 flex items-center justify-center opacity-0 hover:opacity-90">
-                <div className="text-white text-center text-xl">Configurador Ducati</div>
+                <div className="text-white text-center text-xl">Construyendo puentes digitales para un mundo más conectado</div>
               </div>
             </div>
           </div>
@@ -17,7 +17,7 @@ const ImageSection = () => {
             <div className="relative w-full h-64"> 
               <img src="https://i.pinimg.com/564x/44/45/07/444507d60bd86c271a0994cfb78571bb.jpg" alt="Mantenimiento transparente" className="w-full h-full object-cover" />
               <div className="absolute inset-0 bg-black opacity-75 transition-opacity duration-500 flex items-center justify-center opacity-0 hover:opacity-90">
-                <div className="text-white text-center text-xl">Mantenimiento transparente</div>
+                <div className="text-white text-center text-xl">Servicios de consultoría en tecnología: expertos que guían tu éxito.</div>
               </div>
             </div>
           </div>
@@ -25,7 +25,7 @@ const ImageSection = () => {
             <div className="relative w-full h-64"> 
               <img src="https://i.pinimg.com/564x/41/55/cc/4155cce579c9ae3a9399c146251081e2.jpg" alt="Localizador de concesionarios" className="w-full h-full object-cover" />
               <div className="absolute inset-0 bg-black opacity-75 transition-opacity duration-500 flex items-center justify-center opacity-0 hover:opacity-90">
-                <div className="text-white text-center text-xl">Localizador de concesionarios</div>
+                <div className="text-white text-center text-xl">Innovación tecnológica para potenciar tu empresa. ¡Descubre nuestras soluciones hoy!</div>
               </div>
             </div>
           </div>
