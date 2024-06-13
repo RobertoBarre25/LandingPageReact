935e83cd37fd6af7aacb3248817e9c30ca67afbf
Arturo <arturdar16@gmail.com>
2024-06-13 11:10:55 -0600

    cambios

--- src/CardsSer/cards.js
+++ src/CardsSer/cards.js
@@ -36,7 +36,7 @@ const Cards = () => {
         <div className="mx-4 md:mx-12 lg:mx-20">
             <div className="grid grid-cols-2 gap-0">
                 {cardsData.map((card, index) => (
-                    <div key={index} className="relative  h-[850px] border-none group">
+                    <div key={index} className="relative  h-[800px] border-none group">
                         <div className="relative overflow-hidden h-full border-none">
                             <img src={card.imgSrc} alt={card.title} className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-110" />
                             <div className="absolute bottom-2.5 w-full text-center text-white p-2.5 z-10 transition-all duration-300 ease-in-out group-hover:bottom-full">
