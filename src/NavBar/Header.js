e126dd79908d665adc828e98249d325de860f40e
RobertoBarre25 <robegamer25@gmail.com>
2024-06-19 13:01:11 -0600

    NavBar Animation

--- src/NavBar/Header.js
+++ src/NavBar/Header.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import './Header.css'; 
 
 function Header({ isBlue }) {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -24,23 +25,15 @@ function Header({ isBlue }) {
     : scrollPosition > 20 ? 'bg-blue-700' : 'bg-transparent';
 
   return (
-    <header className={`fixed top-0 left-0 w-full z-10 transition-all duration-800 ease-in-out h-20 ${headerClass}`}>
+    <header className={`fixed top-0 left-0 w-full z-10 transition-all duration-800 ease-in-out h-16 ${headerClass}`}>
       <nav className="flex justify-between items-center px-5 h-full">
-        <div className="text-white text-xl md:text-2xl">Grupo Alternativas y Soluciones</div>
-        <ul className="flex space-x-5 list-none">
-          <li>
-            <div style={{ marginTop: '1.3rem' }}>
-              <Link to="/" className="text-white font-bold transition-colors duration-300 hover:bg-blue-700 focus:outline-none bg-transparent p-3 m-3" style={{ textDecoration: 'none', fontSize: '17.5px' }}>
-                Home
-              </Link>
-            </div>
+        <div className="text-white text-lg md:text-xl">Grupo Alternativas y Soluciones</div>
+        <ul className=" text-white flex space-x-5 list-none">
+          <li style={{ marginTop: '1.2rem' }}>
+            <Link to="/" className="nav-link">Home</Link>
           </li>
-          <li>
-            <div style={{ marginTop: '1.3rem' }}>
-              <Link to="/contact" className="text-white font-bold transition-colors duration-300 hover:bg-blue-700 focus:outline-none bg-transparent p-3 m-3" style={{ textDecoration: 'none', fontSize: '17.5px' }}>
-                Contact
-              </Link>
-            </div>
+          <li style={{ marginTop: '1.2rem' }}>
+            <Link to="/contact" className="nav-link">Contact</Link>
           </li>
         </ul>
       </nav>
