c7c36d44362abbcf769981d6c6dde64b173528de
RobertoBarre25 <robegamer25@gmail.com>
2024-06-24 12:48:58 -0600

    scroll form 2

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -3,7 +3,10 @@ import Header from "../NavBar/Header";
 
 const ContactForm = () => {
   return (
-    <div id="contact-form" className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-white-100 to-gray-500 py-10 px-4 sm:px-6 lg:px-8">
+    <div
+      id="contact-form"
+      className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-white-100 to-gray-500 py-10 px-4 sm:px-6 lg:px-8"
+    >
       <Header isBlue={true} />
       <div className="rounded-lg p-8 max-w-2xl w-full bg-transparent">
         <div className="text-center mb-6">
@@ -12,9 +15,7 @@ const ContactForm = () => {
             className="w-32 mx-auto"
             alt="logo"
           />
-          <h2 className="text-2xl font-bold">
-            Solicitar Información del Servicio
-          </h2>
+          <h2 className="text-2xl font-bold">Solicitar Información del Servicio</h2>
         </div>
         <form className="space-y-6">
           <div className="grid gap-6 sm:grid-cols-2">
@@ -34,14 +35,14 @@ const ContactForm = () => {
               required
               type="text"
               className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
-              placeholder="País"
+              placeholder="Dirección"
             />
-            <input
-              required
-              type="text"
-              className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
-              placeholder="Código Postal"
-            />
+             <input
+                required
+                type="email"
+                className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
+                placeholder="Correo Electrónico"
+              />
           </div>
           <div className="grid gap-6 sm:grid-cols-2">
             <input
@@ -63,17 +64,6 @@ const ContactForm = () => {
             <div className="relative">
               <input
                 required
-                type="email"
-                className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
-                placeholder="Correo Electrónico"
-              />
-            </div>
-          </div>
-          
-          <div className="grid gap-6 sm:grid-cols-2">
-            <div className="relative">
-              <input
-                required
                 type="date"
                 className="block w-full px-4 py-2 text-sm border-b-2 border-gray-300 bg-transparent focus:border-blue-600 focus:outline-none"
               />
@@ -88,12 +78,12 @@ const ContactForm = () => {
           </div>
           <div className="relative">
             <label className="block text-sm text-gray-700">
-              ¿Deseas ofrecernos información adicional para ayudarnos a
-              conocerte mejor?
+              ¿Deseas ofrecernos información adicional para ayudarnos a conocerte mejor?
             </label>
             <div className="flex items-center justify-center mt-2">
               <label className="mr-4">
-                <input required
+                <input
+                  required
                   type="radio"
                   name="additionalInfo"
                   className="mr-2"
@@ -101,20 +91,24 @@ const ContactForm = () => {
                 Sí
               </label>
               <label>
-                <input required type="radio" name="additionalInfo" className="mr-2" />
+                <input
+                  required
+                  type="radio"
+                  name="additionalInfo"
+                  className="mr-2"
+                />
                 No
               </label>
             </div>
           </div>
           <div className="relative">
-            <label required className="block text-sm text-gray-700">
+            <label className="block text-sm text-gray-700">
               <strong>Aceptar los términos y condiciones</strong>
             </label>
             <p className="text-sm text-gray-600 mb-2">
-              Antes de continuar, le rogamos leer nuestra nota informativa sobre
-              la privacidad. Después de haber leído y entendido la nota
-              informativa sobre la privacidad, autorizo el tratamiento de mis
-              datos personales por parte de Grupo Alternativas y Solucione:
+              Antes de continuar, le rogamos leer nuestra nota informativa sobre la privacidad.
+              Después de haber leído y entendido la nota informativa sobre la privacidad,
+              autorizo el tratamiento de mis datos personales por parte de Grupo Alternativas y Solucione:
             </p>
             <div className="flex items-start mb-2">
               <input
@@ -123,10 +117,9 @@ const ContactForm = () => {
                 className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
               />
               <span className="ml-2 text-sm text-gray-700">
-                Para actividades de marketing mediante correo electrónico
-                (boletín informativo), teléfono, SMS, MMS, chat, banner en
-                nuestros sitios y aplicaciones, mensajería instantánea, redes
-                sociales y correo tradicional
+                Para actividades de marketing mediante correo electrónico (boletín informativo),
+                teléfono, SMS, MMS, chat, banner en nuestros sitios y aplicaciones,
+                mensajería instantánea, redes sociales y correo tradicional
               </span>
             </div>
             <div className="flex items-start mb-2">
@@ -136,9 +129,8 @@ const ContactForm = () => {
                 className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
               />
               <span className="ml-2 text-sm text-gray-700">
-                Para la creación de perfiles, destinados a la personalización de
-                ofertas e iniciativas de acuerdo con mis intereses y
-                preferencias
+                Para la creación de perfiles, destinados a la personalización de ofertas e
+                iniciativas de acuerdo con mis intereses y preferencias
               </span>
             </div>
           </div>
