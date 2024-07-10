f9e461dfcede6503c5d45e072f3eddc3ebdc2acd
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-07-10 17:41:26 -0600

    carosel

--- src/Components/CarouselComponent.js
+++ src/Components/CarouselComponent.js
@@ -33,17 +33,17 @@ const CarouselComponent = () => {
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Batería</h3>
               <p>Larga vida a la batería.</p>
-              <img src="ruta-a-la-imagen-de-bateria.png" alt="Batería" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Innovación</h3>
               <p>Diseñado para durar. Y enamorar.</p>
-              <img src="ruta-a-la-imagen-del-dispositivo.png" alt="Innovación" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Personaliza tu iPhone</h3>
               <p>Ponle tu estilo. En cada detalle.</p>
-              <img src="https://i.pinimg.com/564x/c7/57/7c/c7577cb0fbd63a7ef1630a54829fae2c.jpg" alt="Personalización" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
           </div>
         </Carousel.Item>
@@ -52,17 +52,17 @@ const CarouselComponent = () => {
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Seguridad</h3>
               <p>Protege tus datos con la mejor tecnología.</p>
-              <img src="ruta-a-la-imagen-de-seguridad.png" alt="Seguridad" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Velocidad</h3>
               <p>Rendimiento superior en cada tarea.</p>
-              <img src="ruta-a-la-imagen-de-velocidad.png" alt="Velocidad" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Calidad de Cámara</h3>
               <p>Fotos y videos de alta calidad.</p>
-              <img src="ruta-a-la-imagen-de-camara.png" alt="Cámara" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
           </div>
         </Carousel.Item>
@@ -71,17 +71,18 @@ const CarouselComponent = () => {
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Durabilidad</h3>
               <p>Construcción resistente y duradera.</p>
-              <img src="ruta-a-la-imagen-de-durabilidad.png" alt="Durabilidad" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Interfaz</h3>
               <p>Interfaz amigable y fácil de usar.</p>
-              <img src="ruta-a-la-imagen-de-interfaz.png" alt="Interfaz" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
             </div>
             <div className="col d-flex flex-column align-items-center justify-content-center m-5 bg-black text-white rounded" style={{ height: '90vh', marginLeft: '30px', marginRight: '30px' }}>
               <h3>Compatibilidad</h3>
               <p>Compatibilidad con múltiples dispositivos.</p>
-              <img src="ruta-a-la-imagen-de-compatibilidad.png" alt="Compatibilidad" className="w-75 rounded" />
+              <img src="https://i.pinimg.com/564x/87/77/f3/8777f3a36f7a8349c2b6882691df7471.jpg" alt="Descripción" className="w-100 img-fluid rounded" />
+
             </div>
           </div>
         </Carousel.Item>
