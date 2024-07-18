b7f884412859597c60fa71e0efe26e96fcfb5c81
Arturo <arturdar16@gmail.com>
2024-07-18 09:44:20 -0600

    errores en servicios de vista reparados

--- src/Services/services.js
+++ src/Services/services.js
@@ -12,19 +12,7 @@ const scrollToMiddle = () => {
 
 function Services() {
     return (
-
-        <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage: "url('https://www.udima.es/sites/udima.es/files/GettyImages-1407650545.jpg')" }}>
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-            <header className="fixed top-0 left-0 w-full z-5 transition-all duration-800 ease-in-out h-16 bg-transparent">
-                {/* Contenido del header */}
-            </header>
-            <div className="relative flex items-center justify-center h-full z-3">
-                <div className="text-center text-white">
-                    <h1 className="text-7xl md:text-9xl font-bold">Grupo Alternativas Soluciones</h1>
-                    <button className="mt-16 px-10 py-6 md:py-8 border border-white text-white text-2xl md:text-3xl">Conoce Nuestros Servicios</button>
-                    <div className="mt-4 flex items-center justify-center">
-                        <div className="text-white text-4xl">
-                            <FontAwesomeIcon icon={faAngleDoubleDown} className="text-white text-4xl animate-bounce mr-2 mt-20" />                       
+                
         <div className="flex flex-col min-h-screen">
             <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: "url('https://www.udima.es/sites/udima.es/files/GettyImages-1407650545.jpg')" }}>
                 <div className="absolute inset-0 bg-black opacity-50"></div>
@@ -78,6 +66,7 @@ function Services() {
                 </div>
             </div>
         </div>
+       
     );
 }
 
