750f39211f3cfed2808b411c6586ad3dbb793f74
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-07-18 09:32:30 -0600

    services

--- src/Services/services.js
+++ src/Services/services.js
@@ -31,9 +31,9 @@ function Services() {
                 </div>
             </div>
             <div className="relative z-5 p-8 flex-grow">
-                <h2 className="text-7xl font-bold text-center text-gray-700 mb-8 mr-4 mt-8 ml-5">Servicios</h2>
+                <h2 className="text-7xl font-bold text-center text-gray-700 mb-16 mr-4 mt-8 ml-5">Servicios</h2>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5 justify-items-center">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                     {[{ place: 'Cámaras de videovigilancia', discount: '-15%', rating: 5, reviews: 10, img: 'https://i.pinimg.com/564x/96/fb/ce/96fbce687cd179fe05c90731e658c7c2.jpg', link: '/camaras' },
                     { place: 'Routers', discount: '-15%', rating: 4, reviews: 8, img: 'https://i.pinimg.com/564x/e3/da/05/e3da05fff94eea57cf8b7395cc6f9979.jpg', link: '/routers' },
                     { place: 'Instalación de fibra', discount: '-15%', rating: 3, reviews: 12, img: 'https://i.pinimg.com/564x/b1/e0/15/b1e0155a8cc3db972b7cb9cb6ffc79f4.jpg', link: '/fibra' },
@@ -47,18 +47,23 @@ function Services() {
                     { place: 'Santorini, Greece', discount: '-15%', rating: 5, reviews: 11, img: 'https://i.pinimg.com/564x/cb/df/89/cbdf8978a85635559958afcd832918cf.jpg', link: '/santorini' },
                     { place: 'Santorini, Greece', discount: '-15%', rating: 5, reviews: 11, img: 'https://i.pinimg.com/564x/cb/df/89/cbdf8978a85635559958afcd832918cf.jpg', link: '/santorini' },
                     ].map((deal, index) => (
-                        <div key={index} className="relative w-96 h-96 bg-cover bg-center overflow-hidden shadow-lg mb-8 mx-4">
-                            <div className="absolute top-0 left-0 bg-red-500 text-white px-2 py-1">{deal.discount}</div>
-                            <img src={deal.img} alt={deal.place} className="w-full h-full object-cover" />
-                            <div className="absolute bottom-4 left-4 right-4  bg-white bg-opacity-75 p-4">
-                                <h3 className="text-lg font-semibold text-black">{deal.place}</h3>
-                                <a href={deal.link} className="text-black underline hover:no-underline">Detalles</a>
+                        <div key={index} className="relative border-none group m-4">
+                            <div className="relative overflow-hidden h-80 w-full border-none">
+                                <div className="absolute inset-0 bg-black opacity-25"></div>
+                                <img src={deal.img} alt={deal.place} className="w-full h-full object-cover" />
+                                <div className="absolute inset-0 flex justify-center items-center">
+                                    <h3 className="text-white text-2xl">{deal.place}</h3>
+                                </div>
+                                <div className="absolute bottom-0 left-0 w-full h-full bg-black bg-opacity-50 text-white p-5 transition-transform duration-700 ease-in-out transform translate-y-full group-hover:translate-y-0 flex justify-center items-end">
+                                    <div className="text-center mb-8">
+                                        <a href={deal.link} className="text-white underline hover:no-underline">Detalles</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
-            
         </div>
     );
 }
