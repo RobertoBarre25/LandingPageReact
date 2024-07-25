8e71a431369d12be788b814ad9af9fb78e89ea63
RobertoBarre25 <robegamer25@gmail.com>
2024-07-24 18:06:07 -0600

    Add Model 3d

--- src/CCTV/Camaras.js
+++ src/CCTV/Camaras.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Container, Row, Col, Button } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+import ReactPlayer from 'react-player';
+import Spline from '@splinetool/react-spline';
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 const scrollToMiddle = () => {
     const targetPosition = window.innerHeight * 1.8;
@@ -159,8 +165,19 @@ const App = () => {
                 <div className="text-center mb-12">
                     <h3 className="text-7xl font-semibold mb-4 text-blue-500">Camaras</h3>
                 </div>
+                <div className="flex flex-col md:flex-row px-4 md:px-6 lg:px-8 w-full py-8 bg-beige">
+                    <div className="text-section flex-1 mb-5 md:mb-0 md:mr-4 flex flex-col justify-center">
+                        <h2 className="text-2xl md:text-3xl font-bold mb-6 text-gray-800">¡Transforma tu infraestructura de TI!</h2>
+                        <p className="text-base md:text-lg mb-6 text-gray-700 leading-relaxed">
+                            ¡Regístrate ahora y descubre ofertas exclusivas en servicios de cableado estructurado! No te pierdas esta oportunidad única para transformar tu empresa. ¡El futuro es tuyo!
+                        </p>
+                        <Button variant="primary" size="sm" className="py-2 px-3">¡Compra Ahora!</Button>
+                    </div>
+                    <div className="spline-section flex-1 flex justify-center items-center">
+                        <Spline scene="https://prod.spline.design/fm6r9Ujx42txyLnz/scene.splinecode" className="w-full h-auto md:h-64" />
+                    </div>
+                </div>
                 <div className="flex flex-wrap justify-center gap-4">
-                    {/* Ejemplo de otros componentes o secciones debajo del carrusel */}
                     <div className="w-full sm:w-1/2 lg:w-1/4 mb-8">
                         <div className="bg-white shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 h-full flex flex-col justify-between mx-6">
                             <div className="p-6 flex-1 flex flex-col justify-between bg-white">
